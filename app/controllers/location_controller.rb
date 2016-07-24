1e865a90b086d1a6950883f4faf5c7150d0890d5
macroscopic <mac.roche@gmail.com>
2016-07-23 23:09:54 -0700

    Mac and Will make forms n routes

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -9,11 +9,22 @@ get '/locations/:location_id' do
   erb :'locations/show'
 end
 
-post '/locations/:location_id/post' do
-  @location = Location.find(params[:location_id])
-  @posts = Post.create(user_id: session[:id], location_id: @location.id, content: params[:content])
+post '/post' do
+  @posts = Post.create(user_id: session[:user_id], location_id: params[:location_id], content: params[:content])
+  # @location = Location.find(params[:location_id])
   # if @post.save
-    redirect "/locations/:location_id"
+    redirect "/locations/#{params[:location_id]}"
+  # else
+  #   @errors = @location.errors.full_messages
+  #   erb :'posts/new'
+  # end
+end
+
+post '/comment' do
+  @comment = Comment.create(user_id: session[:user_id], post_id: params[:post_id])
+  # @location = Location.find(params[:location_id])
+  # if @post.save
+    redirect "/locations/#{params[:location_id]}"
   # else
   #   @errors = @location.errors.full_messages
   #   erb :'posts/new'
