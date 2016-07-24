67eeab867ac50d8c415dfe353590e014afb51bc9
macroscopic <mac.roche@gmail.com>
2016-07-23 21:03:16 -0700

    Create location controllers

---
+++ app/controllers/location_controller.rb
@@ -0,0 +1,21 @@
+get '/locations' do
+  @locations = Location.all
+  erb :'/locations/index'
+end
+
+get '/locations/:location_id' do
+  @location = Location.find(params[:locaction_id])
+  @posts = Post.where(location_id: @location.location_id)
+  erb :'locations/show'
+end
+
+post 'locations/:location_id/post'
+  @location = Location.find(params[:locaction_id])
+  @posts = Post.create(user_id: session[:id], location_id: @location.id, content: params[:content])
+  if @post.save
+    redirect "/locations/#{@location.id}"
+  else
+    @errors = @location.errors.full_messages
+    erb :'posts/new'
+  end
+end
