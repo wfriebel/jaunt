9790940a4a820cc539ef2442ca9b5ff203aeaa36
Martin McLaughlin <email": null>
2016-07-24 12:14:56 -0700

    add functionality to posts page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 post '/comment' do
-  @comment = Comment.create(user_id: session[:user_id], post_id: params[:post_id])
+  if current_user
+    @comment = Comment.create(user_id: session[:user_id], post_id: params[:post_id])
   # @location = Location.find(params[:location_id])
   # if @post.save
     redirect "/locations/#{params[:location_id]}"
+  else
+    redirect "/login"
+  end
   # else
   #   @errors = @location.errors.full_messages
   #   erb :'posts/new'
