d5b843eb7ebf6a94cb47e90e5ca50a101fc6cd5a
macroscopic <mac.roche@gmail.com>
2016-07-24 11:10:11 -0700

    changed spacing in locations migration

--- db/migrate/20160723191650_create_locations.rb
+++ db/migrate/20160723191650_create_locations.rb
@@ -3,7 +3,9 @@ class CreateLocations < ActiveRecord::Migration
   	create_table :locations do |t|
   		t.string :name, null: false
   		t.string :description, null: false
-  		t.timestamps(null: false)
+      
+
+      t.timestamps(null: false)
   	end
   end
 end
