ea1aa2d4fbe87e79c20cf9de8fce8b897fb5840b
Will Friebel <will.friebel@gmail.com>
2016-07-23 20:09:07 -0700

    Create models, migrations, and seed

---
+++ db/migrate/20160723191650_create_locations.rb
@@ -0,0 +1,9 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+  	create_table :locations do |t|
+  		t.string :name, null: false
+  		t.string :description, null: false
+  		t.timestamps(null: false)
+  	end
+  end
+end
